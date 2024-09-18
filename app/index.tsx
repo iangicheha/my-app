ceaa17799e880341805b6fc8e3f90976f7cd1c82
Ian Gicheha <gicheha004@gmail.com>
2024-09-18 12:16:31 +0300

    Update onboarding images, add new fonts, and clean up obsolete assets. Removed old images and added new assets for the onboarding process and fonts.

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,11 @@
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home =() => {
     return(
-        <View>
+        <SafeAreaView>
             <Text>Home</Text>
-        </View>
+        </SafeAreaView>
     );
 };
 
